51034d88d302929c88e6c16b5756b50155fe9213
kkiyama117 <k.kiyama117@gmail.com>
2026-05-13 10:28:32 +0900

    feat(api): re-export SP-2 jobid + plan public surface

--- python/job_manager/__init__.py
+++ python/job_manager/__init__.py
@@ -9,6 +9,17 @@ CalcView = _core.CalcView
 walk_flows = _core.walk_flows
 tick_many = _core.tick_many
 
+# SP-2: jobid helpers
+validate_step_id = _core.validate_step_id
+validate_job_id = _core.validate_job_id
+build_job_id = _core.build_job_id
+parse_job_id = _core.parse_job_id
+
+# SP-2: plan
+ExperimentPlan = _core.ExperimentPlan
+read_plan = _core.read_plan
+write_plan = _core.write_plan
+
 __all__ = [
     "PathResolver",
     "SearchFilter",
@@ -16,4 +27,12 @@ __all__ = [
     "CalcView",
     "walk_flows",
     "tick_many",
+    # SP-2
+    "validate_step_id",
+    "validate_job_id",
+    "build_job_id",
+    "parse_job_id",
+    "ExperimentPlan",
+    "read_plan",
+    "write_plan",
 ]
